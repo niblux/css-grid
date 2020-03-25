0047f09bff040ead8d7f3f20b4e93f81659108b8
Nabil Elidrissi <nabil.elidrissi@live.co.uk>
2020-03-25 19:21:53 +0000

    basic grid fundamentals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-grid">
+      <div className="item">1</div>
+      <div className="item">2</div>
+      <div className="item">3</div>
+
+      <div className="item">4</div>
+      <div className="item">5</div>
+      <div className="item">6</div>
+
+      <div className="item">7</div>
+      <div className="item">8</div>
+      <div className="item">9</div>
+
+      <div className="item">10</div>
+      <div className="item">11</div>
+      <div className="item">12</div>
+
+      {/* <div className="item">13</div>
+      <div className="item">14</div>
+      <div className="item">15</div> */}
+
+      {/* <div className="item">16</div>
+      <div className="item">17</div>
+      <div className="item">18</div> */}
+
+      {/* <div className="item">19</div>
+      <div className="item">20</div>
+      <div className="item">21</div> */}
+
+      {/* <div className="item">19</div>
+      <div className="item">20</div>
+      <div className="item">21</div> */}
     </div>
   );
 }
